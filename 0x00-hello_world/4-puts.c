62007aaef1f69e11b18d2d818fb7b873b1cff31c
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-13 16:58:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly a new line Use the function puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
