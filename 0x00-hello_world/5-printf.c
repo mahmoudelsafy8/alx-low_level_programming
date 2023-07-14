1fd4bca8f645c2dba2974eb4b270e9efef71076a
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-13 17:05:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly a new line Use the function printf
+ * Return: 0 (seccess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
