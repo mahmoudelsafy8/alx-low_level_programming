1b203db9de3a5800d174c27237f433253fbd30df
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 18:42:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + 48);
+	}
+	putchar('\n');
+	return (0);
+}
