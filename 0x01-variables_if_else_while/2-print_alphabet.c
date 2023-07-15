fbba85080c210aab545cbd078839f9dc1c22b178
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 18:08:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
