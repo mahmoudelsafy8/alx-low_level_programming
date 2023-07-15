d91047dfabc9a92baf603c594bae8253a2a3de40
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 18:30:51 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		if (x == 101 || x == 113)
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
