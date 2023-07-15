cf1dc6532827743409251cbfd3c6676008bc513d
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 19:09:29 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+		if (x == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
