70cfe6352fe1278e8a222448aae65cd7fbb9f1af
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 18:48:16 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 122;
+
+	while (x >= 97)
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
