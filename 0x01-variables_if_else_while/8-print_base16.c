9c028b9b47fed1f67e5f53c7b7a6ecba3e46e0ce
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 19:02:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int i;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
