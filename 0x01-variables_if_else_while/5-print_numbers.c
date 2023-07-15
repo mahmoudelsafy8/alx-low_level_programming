02b7aeb0a431daf359d090b0f70b4dbd47e1eaa8
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-14 18:37:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
