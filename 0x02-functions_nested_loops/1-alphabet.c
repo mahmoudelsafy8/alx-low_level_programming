4df5abeb4857ffb33dcb310ac4853ed26d2fee88
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-19 10:33:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry piont
+ * Return: ON Success 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
