a823618d5b0f0e42cfaa30a9bd34b93194eae5f8
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-20 16:45:22 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,26 +7,20 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3) == 0 && (x % 5) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((x % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((x % 5) == 0)
-		{
-			printf("Buzz ");
-		}
+		if (x % 3  == 0 && !(x % 5 == 0))
+			printf("Fizz");
+		else if (x % 5 == 0 && !(x % 3 == 0))
+			printf("Buzz");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
 			printf("%d ", x);
-		}
+		if (x != 100)
+			printf(" ");
+		else
+			printf('\n');
 	}
-	printf("Buzz");
-	printf('\n');
 	return (0);
 }
