f4527c01da38b07d2f858fecfc90593437a9eca7
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-20 16:14:53 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (x % 3 == 0)
+		else if ((x % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (x % 5 == 0)
+		else if ((x % 5) == 0)
 		{
 			printf("Buzz ");
 		}
