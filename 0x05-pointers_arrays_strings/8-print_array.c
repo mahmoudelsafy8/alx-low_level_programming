ebd41de4781ac5c2dbab17d4166c0586fbeda4e3
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-24 15:57:18 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,9 @@ void print_array(int *a, int n)
 	{
 		printf("%d, ", a[x]);
 	}
-		if (x == (n - 1))
-		{
-			printf("%d", a[n - 1]);
-		}
-		printf("\n");
+	if (x == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
 }
