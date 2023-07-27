0a665a2426d902fcc3fdef6e0f5720c9e2ec23b8
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-27 13:46:03 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /*
- * _strcat - appends the src string to the dest string,
- * overwriting the terminating null byte (\0) at the end of dest,
- *  and then adds a terminating null byte.
- *  @dast: input value.
+ * _strcat - appends the src string to the dest string.
+ * _strcat - appends the src string to the dest string.
+ *  @dest: input value.
  *  @src: input value.
  *  Return: pointer to the resulting string dest
  */
@@ -15,6 +14,7 @@ char *_strcat(char *dest, char *src)
 	x = 0;
 	while (dest[x])
 		x++;
+
 	for (y = 0; src[y]; y++)
 		dest[x++] = src[y];
 	return (dest);
