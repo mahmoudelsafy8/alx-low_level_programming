c629a713dcdd1bd38497b0ee5c3443f0c92bc4dc
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-07-31 13:49:11 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: input
+ * @c: input
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	for (; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (&s[x]);
+	}
+	return (0);
+}
