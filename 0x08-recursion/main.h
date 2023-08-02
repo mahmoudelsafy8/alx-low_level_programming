31332fbd0b74db323045b4449286479ea185a097
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-02 15:13:06 -0700

    int _putchar(char c)

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,4 +5,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-#endif /* MAIN_H */
+int _putchar(char c);
