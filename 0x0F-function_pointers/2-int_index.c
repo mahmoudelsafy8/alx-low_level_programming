4e2ccd2d0d0faffc7cbcceee68011524d8e650a1
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-16 10:45:49 -0700

    d

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ * @array: int
+ * @size: size
+ * @cmp: output
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+
+	if (array && size && cmp)
+		while (x < size)
+		{
+			if (cmp(array[x]))
+				return (x);
+			x++;
+		}
+}
