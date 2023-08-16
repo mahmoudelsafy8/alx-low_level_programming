82c501ebe7fe6a7663945ea044fccbb68ac9a205
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-16 11:40:29 -0700

    g

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,7 +1,9 @@
 #ifndef CALC_H
 #define CALC_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * struct op - Struct op
  * @op: The operator
@@ -19,4 +21,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "3-clac.h"
+#include "3-calc.h"
 /**
  * get_op_func - return a pointer to the function
  * @s: str
