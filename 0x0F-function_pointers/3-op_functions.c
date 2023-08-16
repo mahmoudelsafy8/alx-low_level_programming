7be54d67d190451954c343a8e5af726184d93af3
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-16 11:33:44 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-clac.h"
+/**
+ * op_add - adds int
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub int
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul int
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div int
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod int
+ * @a: int
+ * @b: int
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
