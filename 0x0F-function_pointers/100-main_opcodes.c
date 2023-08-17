41ca842bc29bbf6e463d24973381c8ca728127a7
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-17 15:23:23 -0700

    e

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: number
+ * @argv: number
+ * Return: 0
+ */
+int main(argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
