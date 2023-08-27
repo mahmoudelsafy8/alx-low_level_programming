8dbdffe1b74d515b1a235625b14d48a9f69180af
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2023-08-27 11:24:21 -0700

    g

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * first - print sentence before the main function is executed.
  */
-void first(void)
+void first(void)__attribute__ ((constructor))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("bore my house upon my back!\n");
