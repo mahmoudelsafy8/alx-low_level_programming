564b73a6666c6b60a120325009783b52da55e6b8
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-01-12 04:13:52 -0800

    2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print number of elements in a linked dlistint_t list
+ * dlistint_len - print number of elements in a linked dlistint_t list
  * @h: head of list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 
