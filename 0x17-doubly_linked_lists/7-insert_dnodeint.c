e02bd778dbafb6464696445d47fa7831225f656c
mahmoudelsafy8 <mahmoudelsafy8@gmail.com>
2024-01-12 10:24:14 -0800

    8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *mp = *h, *new;
 
 	if (idx == 0)
-		return (add_dlistint(h, n));
+		return (add_dnodeint(h, n));
 	for (; idx != 1; idx--)
 	{
 		mp = mp->next;
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	if (mp->next == NULL)
-		return (add_dlistint_end(h, n));
+		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *mp - *head;
+	dlistint_t *mp = *head;
 
 	if (*head == NULL)
 		return (-1);
